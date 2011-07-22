d5bc14c6431cfe78b55c26288bfd1b8ae7a4d911
Dave Huseby <huseby@lindenlab.com>
2011-07-22 10:25:41 -0700

    starting the 3p-icu4c library

---
+++ build-cmd.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# turn on verbose debugging output for parabuild logs.
+set -x
+# make errors fatal
+set -e
+
+ICU4C_VERSION="4.8.1"
+ICU4C_VERSION="zlib-$ICU4C_VERSION"
+
+if [ -z "$AUTOBUILD" ] ; then 
+    fail
+fi
+
+if [ "$OSTYPE" = "cygwin" ] ; then
+    export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
+fi
+
+# load autbuild provided shell functions and variables
+set +x
+eval "$("$AUTOBUILD" source_environment)"
+set -x
+
+stage="$(pwd)/stage"
+pushd "$ICU4C_SOURCE_DIR"
+    case "$AUTOBUILD_PLATFORM" in
+        "windows")
+            #load_vsvars
+            #
+            #pushd contrib/masmx86
+            #    ./bld_ml32.bat
+            #popd
+            # 
+            #build_sln "contrib/vstudio/vc10/zlibvc.sln" "Debug|Win32" "zlibstat"
+            #build_sln "contrib/vstudio/vc10/zlibvc.sln" "Release|Win32" "zlibstat"
+            #mkdir -p "$stage/lib/debug"
+            #mkdir -p "$stage/lib/release"
+            #cp "contrib/vstudio/vc10/x86/ZlibStatDebug/zlibstat.lib" \
+            #    "$stage/lib/debug/zlibd.lib"
+            #cp "contrib/vstudio/vc10/x86/ZlibStatRelease/zlibstat.lib" \
+            #    "$stage/lib/release/zlib.lib"
+            #mkdir -p "$stage/include/zlib"
+            #cp {zlib.h,zconf.h} "$stage/include/zlib"
+        ;;
+        "darwin")
+            #./configure --prefix="$stage"
+            #make
+            #make install
+			#mkdir -p "$stage/include/zlib"
+			#mv "$stage/include/"*.h "$stage/include/zlib/"
+        ;;
+        "linux")
+            #CFLAGS="-m32" CXXFLAGS="-m32" ./configure --prefix="$stage"
+            #make
+            #make install
+            #mkdir -p "$stage/include/zlib"
+            #mv "$stage/include/"*.h "$stage/include/zlib/"
+			#
+            #mv "$stage/lib" "$stage/release"
+            #mkdir -p "$stage/lib"
+            #mv "$stage/release" "$stage/lib"
+        ;;
+    esac
+    #mkdir -p "$stage/LICENSES"
+    #tail -n 31 README > "$stage/LICENSES/zlib.txt"
+popd
+
+pass
+
