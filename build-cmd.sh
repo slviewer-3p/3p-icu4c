810198ea447b0bafd8b33a35e0a304dd3ea06b3d
Logan Dethrow <log@lindenlab.com>
2011-11-03 11:26:30 -0400

    Added windows build configuration to build-cmd.sh. Also added include to common manifest and enabled windows build in Buildparams.

--- build-cmd.sh
+++ build-cmd.sh
@@ -27,7 +27,28 @@ stage=$(pwd)/stage
 pushd "$ICU4C_SOURCE_DIR"
     case "$AUTOBUILD_PLATFORM" in
         "windows")
-            #load_vsvars
+            load_vsvars
+            
+            # According to the icu build instructions for Windows, 
+            # runConfigureICU doesn't work for the Microsoft build tools, so
+            # just use the provided .sln file.
+            
+            pushd ../icu/source
+                build_sln "allinone\allinone.sln" "Release|Win32"
+                build_sln "allinone\allinone.sln" "Debug|Win32"
+            popd
+                       
+            mkdir -p "$stage/lib/debug"
+            mkdir -p "$stage/lib/release"
+            mkdir -p "$stage/include"
+            
+            # Copy the .lib files that don't have a "d" on the end to release
+            find ./lib -regex '\./icu.*[^d]\.lib' -exec cp {} $stage/lib/release \;
+            # Copy the .lib files and .pdb files ending in "d" to debug
+            find ./lib -regex '\./icu.*d\.{lib,pdb}' -exec cp {} $stage/lib/debug \;
+            
+            cp -R include/* "$stage/include"
+    
         ;;
         "darwin")
             #./configure --prefix="$stage"
